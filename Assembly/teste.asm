922ed5016f357476553470f65184e6bfd35449ff
Henrique-Versiani <henriquecversiani@gmail.com>
2023-04-27 19:05:53 -0300

    Opção 3 completa

--- Assembly/teste.asm
+++ Assembly/teste.asm
@@ -1,18 +1,17 @@
 .data
-	OPCAO1: .asciiz "\n\nOPCÂO 1:\n"
-	OPCAO2: .asciiz "OPCÂO 2:\n"
-	OPCAO3: .asciiz "OPCÂO 3:\n"
-	insira: .asciiz "Digite uma string: "
+	OPCAO1: .asciiz "\n\nOPCÂO 1:\n"				#Lê uma string e informa se é um palindromo ou não
+	OPCAO2: .asciiz "OPCÂO 2:\n"					#Lê uma string e informa a quantidade de caracteres maiúsculos e minúsculos
+	OPCAO3: .asciiz "OPCÂO 3:\n"					#Lê uma string e um caractere e informa quantos do caractere escolhido existem na string informada
+	insira_string: .asciiz "\nDigite uma string: "
+	insira_caractere: .asciiz "Digite um caractere: "
 	maiuscula: .asciiz "Quantidade de letras maiúsculas: "
 	minuscula: .asciiz "\nQuantidade de letras minúsculas: "
-	new_line: .asciiz "\n"
+	contagem_igual: .asciiz "\nQuantidade do caractere informado na string: "
 	SAIR: .asciiz "OPCÂO 4(SAIR DO PROGRAMA):\n"
 	escolha: .asciiz
 	espaco: .space 30
 	
 .text
-
-
 
 menuloop:
 	li $v0, 4
@@ -36,12 +35,19 @@ menuloop:
 	beq $v0, 3, Contagem_caracter_escolhido
 	beq $v0, 4, FIM
 	
-Palindromo:       #--------------OPCÃO 1--------------------
+Palindromo:      					 #--------------OPCÃO 1--------------------
+	
+	
+	
+	
 	
 	j menuloop
-Quantidade_de_caracteres:	#--------------OPCÃO 2--------------------
+	
+	
+	
+Quantidade_de_caracteres:				#--------------OPCÃO 2--------------------
 	li $v0, 4
-	la $a0, insira
+	la $a0, insira_string
 	syscall
 	
 	#Lê a string inserida pelo usuário
@@ -86,9 +92,56 @@ Quantidade_de_caracteres:	#--------------OPCÃO 2--------------------
 		move $a0, $t1
 		syscall
 		j menuloop	
-Contagem_caracter_escolhido:	#--------------OPCÃO 3--------------------
+		
+		
+		
+Contagem_caracter_escolhido:				#--------------OPCÃO 3--------------------
+
+	li $v0, 4
+	la $a0, insira_caractere
+	syscall
 	
-	j menuloop
+#Lê o caractere inserido pelo usuário
+	li $v0, 12
+	syscall
+	move $t1, $v0 	#endereço caractere
+	
+	li $v0, 4
+	la $a0, insira_string
+	syscall
+	
+#Lê a string inserida pelo usuário
+	li $v0, 8
+	la $a0, 0($sp)
+	li $a1, 30
+	syscall
+	move $t2, $a0	#endereço da string
+
+	li $t0, 0	#contador
+	
+	contagem:
+		lb $t3, ($t2)
+		beq $t3, 10, imprime2
+		beq $t3, $t1, igual
+		addi $t2, $t2, 1
+		j contagem
+	
+	igual:
+		addi $t0, $t0, 1
+		addi $t2, $t2, 1
+		j contagem
+	
+	imprime2:
+		li $v0, 4
+		la $a0, contagem_igual
+		syscall
+		li $v0, 1
+		move $a0, $t0
+		syscall
+		j menuloop
+			
+		
+		
 FIM:
 	li $v0,10
 	syscall 
